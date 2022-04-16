7b25ffc626037695464e08912363c70517d7c598
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-04-16 15:28:39 -0400

    added in checklist

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,18 @@
+
+import React from 'react';
 import './App.css';
+import Checklist from './components/Checklist'
 
 function App() {
   return (
     <div className="main">
       <div id="left">
-
+          <Checklist />
       </div>
       <div id="middle">
-
       </div>
       <div id="right">
-
+        
       </div>
     </div>
   );
--- my-app/src/components/Checklist.js
+++ my-app/src/components/Checklist.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
+import ChecklistItem from './ChecklistItem';
 
 function Checklist(props){
   const[list,setList] = useState([]);
+  const[value,setValue] = useState("");
+
+  const _handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setList([...list, value]);
+      setValue("");
+    }
+  }
 
   return(
     <div id="checklist">
-
+      <div className="title">Your CheckList:</div>
+      <div className="list-container">
+        {list.map((item, index) => (
+         <ChecklistItem checked = {false} value = {item}/>
+        ))}
+        <input onChange={(e) => { setValue(e.target.value);}}  onKeyDown={_handleKeyDown} value={value} placeholder="Input New Item Here"/>
+    </div>
     </div>
   );
 }
+
+export default Checklist;
