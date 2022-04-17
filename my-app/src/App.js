c530c92012e03f70d44b4e4dd0e001226c67061a
lizfrey <elizabeth.w.frey.24@dartmouth.edu>
2022-04-16 21:34:28 -0400

    somethiing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 
-import React from 'react';
 import './App.css';
 import React, {useState} from "react";
 import Animal from './components/Animal.js'
@@ -7,24 +6,29 @@ import ChooseBuddy from './components/ChooseBuddy';
 import Time from './components/Time.js'
 import Checklist from './components/Checklist'
 import Suggestions from './components/suggestions/suggestions.js';
+import app from './firestore';
+import { Auth } from './firestore/Auth';
 
 function App() {
   const [value, setValue] = useState("cat");
 
   return (
-    <div className="main">
-      <div id="left">
-          <Checklist />
+    <Auth>
+      <div className="main">
+        <button onClick={()=> app.auth().signOut()}>Sign</button>
+        <div id="left">
+            <Checklist />
+        </div>
+        <div id="middle">
+          <ChooseBuddy setValue = {setValue}/>
+          <Time />
+          <Animal animalType = {value}/>
+        </div>
+        <div id="right">
+          <Suggestions />
+        </div>
       </div>
-      <div id="middle">
-        <ChooseBuddy setValue = {setValue}/>
-        <Time />
-        <Animal animalType = {value}/>
-      </div>
-      <div id="right">
-        <Suggestions />
-      </div>
-    </div>
+    </Auth>
   );
 }
 
