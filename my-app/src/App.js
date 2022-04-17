f42dbedf137df733a0a4b0c805adc35a8515fb52
r-chai <russellchai2002@gmail.com>
2022-04-16 20:00:32 -0400

    what i have so far for button selection

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-
+import React, {useState} from "react";
+import Animal from './components/Animal.js'
+import ChooseBuddy from './components/ChooseBuddy';
+import Time from './components/Time.js'
 function App() {
+  const [value, setValue] = useState("cat");
+
   return (
     <div className="main">
       <div id="left">
-
       </div>
       <div id="middle">
-
-      </div>
+        <ChooseBuddy setValue = {setValue}/>
+        <Time />
+        <Animal animalType = {value}/>
+      </div> 
       <div id="right">
 
       </div>
