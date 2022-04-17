43360ec7d6df0a7121365dc9206d0b6bd44507fd
Kevin King <kevin.m.king@icloud.com>
2022-04-17 05:12:16 -0400

    heads on menu

--- my-app/src/components/menu/SlidingMenu.js
+++ my-app/src/components/menu/SlidingMenu.js
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import cx from "classnames";
 import styles from "./SlidingMenu.module.css";
+import catHeadPhoto from "../../img/cat-head.svg"
+import dogHeadPhoto from "../../img/dog-head.svg"
+import pandaHeadPhoto from "../../img/panda-head.svg"
 
 const SlidingMenu = props => {
   const { isOpen, children, onChange } = props;
@@ -26,7 +29,26 @@ const SlidingMenu = props => {
       {/* sliding menu */}
       <div className={cx(styles.menu, { [styles.active]: !isOpen })}>
         {children}
-        
+        <div className='animal-button'> 
+            <div className='animal-head'>
+                <label>
+                    <img src = {catHeadPhoto} alt = "cat head" />
+                    <input type="radio" name="avatar" value="cat" onChange={(e)=>{this.onChangeValue(e);}}/>
+                </label>
+            </div>   
+            <div className='animal-head'>
+                <label>
+                    <img src = {dogHeadPhoto} alt = "dog head" />
+                    <input type="radio" name="avatar" value="dog" onChange={(e)=>{this.onChangeValue(e);}}/>
+                </label>  
+            </div>
+            <div className='animal-head'>
+                <label>
+                    <img src = {pandaHeadPhoto} alt = "panda head" />
+                    <input type="radio" name="avatar" value="panda" onChange={(e)=>{this.onChangeValue(e);}}/>
+                </label>
+            </div> 
+        </div>
       </div>
     </div>
   );
