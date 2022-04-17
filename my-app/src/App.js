e33c3ba04487ff95e8d037daacb639d751814d32
Annie Qiu <annie.r.qiu.24@dartmouth.edu>
2022-04-16 20:03:46 -0400

    merging

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-
+import Animal from './components/Animal';
 import Checklist from './components/Checklist'
 import Suggestions from './components/suggestions/suggestions.js';
 
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <div className="main">
-      <h1>Hello</h1>
 
       <div id="left">
           <Checklist />
       </div>
       <div id="middle">
+          <Animal animalType={'cat'}/>
       </div>
       <div id="right">
         <Suggestions />
